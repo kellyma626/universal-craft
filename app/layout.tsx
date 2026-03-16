f322cb05d7fca09cdbe921c96882a31662134635
kellyma626 <kellyma.dev@gmail.com>
2026-03-16 14:09:20 -0700

    feat: clean up homepage placeholder and fix dark mode [UC-20]

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ colorScheme: 'light' }}>
       <body className={`${inter.variable} ${pinyon.variable} antialiased`}>
         {children}
       </body>
