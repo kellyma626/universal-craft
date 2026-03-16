cbe917ff7ef41f56227b46e51f9a71ec165adf25
kellyma626 <kellyma.dev@gmail.com>
2026-03-15 21:11:42 -0700

    fix: rewrite eslint config without FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,8 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 import prettier from 'eslint-config-prettier';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
+import next from 'eslint-config-next';
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals'),
+  ...next.configs['core-web-vitals'],
   prettier,
   {
     rules: {
