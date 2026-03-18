900c4a256e9cae3ff49f578cffa2496f6f9cf8ee
kellyma626 <kellyma.dev@gmail.com>
2026-03-18 14:31:38 -0700

    feat: add catalog page [UC-7]

---
+++ app/catalog/page.tsx
@@ -0,0 +1,11 @@
+import FeaturedProducts from '@/components/FeaturedProducts';
+import InStoreProducts from '@/components/InStoreProducts';
+
+export default function CatalogPage() {
+  return (
+    <main>
+      <FeaturedProducts />
+      <InStoreProducts />
+    </main>
+  );
+}
